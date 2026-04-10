85aedc902e22dee979a24ebd951969781cab5815
Júlio César Pires Miranda dos Santos <julio.ds.2025@gmail.com>
2026-04-10 11:39:16 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -2,18 +2,22 @@ let filmes = [];
 
 //Cadastro
 document.getElementById("btnCadastrar").addEventListener("click", cadastrarFilme);
+//document é o "tronco principal" que te dá acesso a todas as "folhas", podendo achar e mudar coisas no html.
+//getElementById é a maneira de identificar o elemento pelo id do html para que voce possa mudar elas.
 
 //Filtros
 document.getElementById("btnFiltrarClassificacao").addEventListener("click", filtrarClassificacao);
 document.getElementById("btnFiltrarAno").addEventListener("click", filtrarAno);
 
 //Funções
-function cadastrarFilme() {
-    let titulo = document.getElementById("titulo").value;
-    let ano = document.getElementById("ano").value;
-    let classificacao = document.getElementById("classificacao").value;
 
     //Validação
+function cadastrarFilme() {
+    let titulo = document.getElementById("titulo").value;    
+    let ano = document.getElementById("ano").value;
+    let classificacao = document.getElementById("classificacao").value;
+    //value serve para pegar informação que está guardada dentro dele.
+    
     if (titulo === "" || ano === "" || classificacao === "") {
         alert("Preencha todos os campos");
         return;
@@ -26,12 +30,15 @@ function cadastrarFilme() {
     };
 
     filmes.push(filme);
+    //push é um metodo usado para adicionar um item ao final de uma lista
     limparCampos();
     exibirFilmes(filmes);
 }
 
 function exibirFilmes(lista) {
     let texto = lista.map(f => `${f.titulo} (${f.ano}) - Classificação: ${f.classificacao}`).join("<br>");
+    //map é um comando usado para transformar todos os itens de uma lista (array) de uma vez só
+    //join é um comando usado para juntar todos os itens de uma lista (array) e transformá-los em uma única frase
     document.getElementById("listaFilmes").innerHTML = texto;
 }
 
@@ -42,7 +49,6 @@ function limparCampos() {
 }
 
 //Filtros
-//
 function filtrarClassificacao() {
     let filtro = document.getElementById("filtroClassificacao").value;
     let resultado = filmes.filter(f => f.classificacao === filtro)
